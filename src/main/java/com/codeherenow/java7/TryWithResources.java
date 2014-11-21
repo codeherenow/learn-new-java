d33be96ee805b089ad108dee5911183f7c78640d
Ragunath Jawahar <rj@mobsandgeeks.com>
2014-11-21 20:07:38 +0530

    [Example] Java 7, try-with-resources.

---
+++ src/main/java/com/codeherenow/java7/TryWithResources.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2014, Ragunath Jawahar (www.codeherenow.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codeherenow.java7;
+
+import java.io.*;
+
+/**
+ * @author Ragunath Jawahar <www.codeherenow.com>
+ */
+
+public class TryWithResources {
+
+    public static void main(String[] args) {
+        File file = new File("files/try-with-resources.txt");
+
+        /*
+         * The 'try-with-resources' block takes care of properly closing the resources specified within
+         * the `resource specification header`. These resources must implement the {@link java.lang.AutoCloseable}
+         * interface or extend one of the implementing classes.
+         *
+         * Refer: https://docs.oracle.com/javase/7/docs/api/java/lang/AutoCloseable.html
+         */
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            // Read file contents
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+
+            // Print the file contents
+            System.out.println(stringBuilder);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*
+         * Prior to Java 7, resources must be explicitly closed using a 'finally' block.
+         */
+        /*
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
+
+            // Read file contents
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+
+            // Print the file contents
+            System.out.println(stringBuilder);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Close all resources in the opposite order in which they were created
+            try {
+                if (bufferedReader != null) bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (fileReader != null) fileReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        */
+    }
+
+}
